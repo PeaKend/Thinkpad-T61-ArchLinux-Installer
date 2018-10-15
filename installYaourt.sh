5acc37db0b789cef6acfa2bba7ff8cb849d21724
Facundo Atrio <peakend22@gmail.com>
2018-10-15 08:57:55 -0300

    added a yaourt installation script

---
+++ installYaourt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $EUID -eq 0 ]]; then
+	echo "you need to run this script as the user"
+	exit 1
+fi
+
+
+## Installs yaourt
+
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+rm -rf package-query
+rm -rf yaourt
