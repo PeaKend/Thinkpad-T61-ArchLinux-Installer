b6fe3171d8bfcb1f5722f3564a21cf36955c3a71
Facundo Atrio <peakend22@gmail.com>
2018-11-10 15:51:26 -0300

    organized code

--- install.sh
+++ install.sh
@@ -33,30 +33,9 @@ printf "\n$userName ALL=(ALL) ALL" >> /etc/sudoers
 
 clear
 
-while true; do
-	printf "Choose your DE/WM\n\n"
+scripts/environment-choose.sh
 
-	printf "[0] No DE/WM\n"
-	printf "[1] i3\n"
-	printf "[2] KDE Plasma\n"
-	printf "[3] KDE Plasma (without kde-applications)\n"
-	printf "[4] KDE Plasma Minimal\n"
-	printf "[5] Gnome\n"
-	printf "[6] Gnome (without gnome-extra)\n\n"
-
-    printf "Your choice: "
-	read deChoice
-
-	if [ "${deChoice}" -ge "0" ] && [ "${deChoice}" -le "6" ]; then
-		break
-	fi
-
-	clear
-	printf "ERROR: choose a number in the list below\n\n"
-
-done
-
-clear
+deChoice=(<deChoice)
 
 printf "Setting timezone (Argentina)\n\n"
 
---
+++ scripts/environment-choose.sh
@@ -0,0 +1,26 @@
+clear
+
+while true; do
+	printf "Choose your DE/WM\n\n"
+
+	printf "[0] No DE/WM\n"
+	printf "[1] i3\n"
+	printf "[2] KDE Plasma\n"
+	printf "[3] KDE Plasma (without kde-applications)\n"
+	printf "[4] KDE Plasma Minimal\n"
+	printf "[5] Gnome\n"
+	printf "[6] Gnome (without gnome-extra)\n\n"
+
+    printf "Your choice: "
+	read deChoice
+
+	if [ "${deChoice}" -ge "0" ] && [ "${deChoice}" -le "6" ]; then
+		printf "${deChoice}" > ../deChoice
+		break
+	fi
+
+	clear
+	printf "ERROR: choose a number in the list below\n\n"
+
+done
+
